fabbb5ea3e81bc6507bca6cef28038a7026f5c7c
nikhilsangwan007 <nikhil.sangwan007@gmail.com>
2017-02-09 02:24:57 +0530

    add server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello king legend!')
+  res.sendfile('./public/index.html');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
 
