8a28f8fddb8b1acc56a27becda7cf0245368be9b
nikhilsangwan007 <nikhil.sangwan007@gmail.com>
2017-02-09 08:56:39 +0530

    adding toolbar

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendfile('./public/index.html');
+  res.render('index.html');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+});
 
