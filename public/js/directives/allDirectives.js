573eb817269da1d46e65a0f2424b5892b168e431
nikhilsangwan007 <nikhil.sangwan007@gmail.com>
2017-02-24 14:14:54 +0530

    added reponsiveness

--- public/js/directives/allDirectives.js
+++ public/js/directives/allDirectives.js
@@ -3,20 +3,20 @@ angular.module('allDirectives', [])
 	.directive('navBar', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '../views/nav-bar.html'
+			templateUrl: '../views/directive-templates/nav-bar.html'
 		};
 	})
 
 	.directive('sideNav', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '../views/side-nav.html'
+			templateUrl: '../views/directive-templates/side-nav.html'
 		}
 	})
 
 	.directive('hiddenSideNav', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '../views/hidden-side-nav.html'
+			templateUrl: '../views/directive-templates/hidden-side-nav.html'
 		}
 	})
