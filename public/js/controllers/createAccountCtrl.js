e3fe88ee0aec70ff10a00b6c34d793510fa4a549
Nikhil Sangwan <nikhil.sangwan95@gmail.com>
2017-03-25 09:36:39 +0530

    create-account functionality

--- public/js/controllers/createAccountCtrl.js
+++ public/js/controllers/createAccountCtrl.js
@@ -1,5 +1,18 @@
 angular.module('createAccountCtrl', [])
 
-	.controller('createAccountCtrl', ['$scope', 'signupService', function($scope, signupService) {
-		
+	.controller('createAccountCtrl', ['$scope', '$location', 'signupService', function($scope, $location, signupService) {
+		$scope.createAccount = function() {
+			var data = {
+					"username" : $scope.user.email,
+					"password" : $scope.user.password 
+				};
+				console.log(data);
+				signupService.userSignup(data)
+					.success(function(data, status, headers, config) {
+						console.log(data);
+						$location.path('/home');
+					}).error(function(){
+
+					})
+		}
 	}])
