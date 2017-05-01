9e6e9fbcc913de8ba16a0ab8de135929cc88ac1d
rajat17sharma <rajatdevsharma03@gmail.com>
2017-05-01 22:17:42 +0530

    Testing for addPost.Not ready for test.

--- public/js/controllers/addPostCtrl.js
+++ public/js/controllers/addPostCtrl.js
@@ -2,7 +2,19 @@
 	'use strict';
 	angular.module('addPostCtrl', [])
 
-	.controller('addPostCtrl', function($scope, $rootScope) {
-		
-	})
+	.controller('addPostCtrl', function($scope, $rootScope,$http) {
+		var data={};
+		data.title1= $scope.apiName;
+		data.desc= $scope.apiDescription;
+		data.link1= $scope.apiLink;
+		data.tags1= $scope.apiTags;
+		$scope.submitform=function(){
+			$http.post("/api/addPost",data)
+			.then(function(data	){
+				console.log(data);
+				console.log("post added successfully");
+
+			});	
+		}
+	});
 })();
