45805de847bd4870cdf9efaa3501a04ce231ea25
lovingyugs <lovingyugs95@gmail.com>
2017-05-02 00:44:52 +0530

    updated config.json

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
 
+var database;
+if(process.env.NODE_ENV === 'development'){
+	database:'mongodb://summonapi:TUlnictUrtuldA7@ds115671.mlab.com:15671/summonapi'
+}
+else if(process.env.NODE_ENV === 'production'){
+	database:'mongodb://summonapi:TUlnictUrtuldA7@ds115671.mlab.com:15671/summonapi'
+}
+else{
+	 database = 'mongodb//localhost:27017/summonapi',
+}
+
 module.exports={
 
-	'database':'mongodb://localhost:27017/summonAPI',
-	'port':process.env.PORT || 8000,
+	'database':database,
+	'port':process.env.NODE_ENV || 8000,
 	'secret': 'mySummon',
 	'passwordKey':'mySummon',
-	'path':__dirname
+	'path':__dirname,
 	//"secretKey":"Yoursecretkey"
 }
