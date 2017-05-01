36d77fbe720d3d49e19b5f84c45fd00a36818b8d
nikhilsangwan007 <nikhil.sangwan95@gmail.com>
2017-05-02 04:00:11 +0530

    redesign cards add some css to post-details page

--- public/js/controllers/postDetailCtrl.js
+++ public/js/controllers/postDetailCtrl.js
@@ -5,7 +5,10 @@
 			console.log($routeParams);
 			postDetailService.getPostDetails($routeParams)
 				.then(function(resp) {
-					console.log(resp);
+					$scope.detailedData = resp.data;
+					$scope.private_post = resp.data.post.private_post;
+					$scope.public_post = resp.data.post.public_post;
+					console.log(resp.data);
 				}, function errorCallback(err) {
 					console.log('error occured', err);
 				});
