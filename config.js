7b67bc523a7580983e7f795ee8f27b7d82c4bb96
lovingyugs <lovingyugs95@gmail.com>
2017-05-02 01:02:21 +0530

    Updated env variables.

--- config.js
+++ config.js
@@ -1,21 +1,23 @@
 
-var database;
+var database ;
 if(process.env.NODE_ENV === 'development'){
-	database:'mongodb://summonapi:TUlnictUrtuldA7@ds115671.mlab.com:15671/summonapi'
+	console.log("here1");
+	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds115671.mlab.com:15671/summonapi';	
 }
 else if(process.env.NODE_ENV === 'production'){
-	database:'mongodb://summonapi:TUlnictUrtuldA7@ds115671.mlab.com:15671/summonapi'
+	console.log("here2");
+	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds115671.mlab.com:15671/summonapi'
 }
 else{
-	 database = 'mongodb//localhost:27017/summonapi',
+	console.log("here3");
+	database = 'mongodb://localhost:27017/summonAPI'
 }
-
 module.exports={
 
 	'database':database,
-	'port':process.env.NODE_ENV || 8000,
+	'port':process.env.PORT || 8000,
 	'secret': 'mySummon',
 	'passwordKey':'mySummon',
-	'path':__dirname,
+	'path':__dirname
 	//"secretKey":"Yoursecretkey"
 }
