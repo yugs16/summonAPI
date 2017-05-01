22ec6dbf204961ab1781f46f8ce8d9c2e007eb5c
lovingyugs <lovingyugs95@gmail.com>
2017-05-02 01:10:02 +0530

    Updated config.json

--- config.js
+++ config.js
@@ -2,15 +2,16 @@
 var database ;
 if(process.env.NODE_ENV === 'development'){
 	console.log("here1");
-	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds115671.mlab.com:15671/summonapi';	
+	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds127801.mlab.com:27801/summonapi';	
+	mongodb://<dbuser>:<dbpassword>@ds127801.mlab.com:27801/summonapi
 }
 else if(process.env.NODE_ENV === 'production'){
 	console.log("here2");
-	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds115671.mlab.com:15671/summonapi'
+	database = 'mongodb://summonapi:MEwCatAykAcAxA1@ds127801.mlab.com:27801/summonapi'
 }
 else{
 	console.log("here3");
-	database = 'mongodb://localhost:27017/summonAPI'
+	database = 'mongodb://localhost:27017/summonapi'
 }
 module.exports={
 
